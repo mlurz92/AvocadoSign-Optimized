dd0babe94da477d9ffff2a377537eb6ece10d0af
Markus Lurz <72194266+mlurz92@users.noreply.github.com>
2025-06-06 18:57:13 +0200

    Update chart_renderer.js

--- js/ui/renderers/chart_renderer.js
+++ js/ui/renderers/chart_renderer.js
@@ -2,26 +2,9 @@ const chartRenderer = (() => {
 
     const C = APP_CONFIG.CHART_SETTINGS;
 
-    function _applyRadiologyStyle(selection, type) {
-        switch (type) {
-            case 'axis-label':
-                selection.style('font-size', C.AXIS_LABEL_FONT_SIZE).style('font-family', 'sans-serif').attr('fill', C.UI_SETTINGS.ICON_COLOR);
-                break;
-            case 'axis-tick':
-                selection.style('font-size', C.TICK_LABEL_FONT_SIZE).style('font-family', 'sans-serif').attr('fill', C.UI_SETTINGS.ICON_COLOR);
-                break;
-            case 'legend':
-                selection.style('font-size', C.LEGEND_FONT_SIZE).style('font-family', 'sans-serif').attr('fill', C.UI_SETTINGS.ICON_COLOR);
-                break;
-            case 'tooltip':
-                selection.style('font-size', C.TOOLTIP_FONT_SIZE).style('font-family', 'sans-serif').style('background-color', 'rgba(255,255,255,0.9)').style('border', '1px solid #ddd').style('padding', '8px').style('border-radius', '4px').style('pointer-events', 'none').style('position', 'absolute').style('z-index', '10').style('opacity', '0').style('transition', 'opacity 0.2s');
-                break;
-        }
-    }
-
     function _createBaseSvg(containerId, config) {
-        d3.select(containerId).html('');
-        const svg = d3.select(containerId)
+        d3.select(`#${containerId}`).html('');
+        const svg = d3.select(`#${containerId}`)
             .append("svg")
             .attr("width", config.width + config.margin.left + config.margin.right)
             .attr("height", config.height + config.margin.top + config.margin.bottom)
@@ -31,201 +14,206 @@ const chartRenderer = (() => {
     }
 
     function _createTooltip(containerId) {
-        return d3.select(containerId)
-            .append("div")
-            .attr("class", "chart-tooltip")
-            .call(_applyRadiologyStyle, 'tooltip');
+        let tooltip = d3.select(`#${containerId}-tooltip`);
+        if (tooltip.empty()) {
+            tooltip = d3.select("body").append("div")
+                .attr("id", `${containerId}-tooltip`)
+                .attr("class", "chart-tooltip")
+                .style("opacity", 0)
+                .style("position", "absolute")
+                .style("pointer-events", "none");
+        }
+        return tooltip;
     }
 
-    function renderAgeDistribution(containerId, data) {
-        const config = { width: C.DEFAULT_WIDTH, height: C.DEFAULT_HEIGHT, margin: C.DEFAULT_MARGIN };
+    function renderBarChart(containerId, data, options = {}) {
+        const config = {
+            width: options.width || C.DEFAULT_WIDTH,
+            height: options.height || C.DEFAULT_HEIGHT,
+            margin: options.margin || C.DEFAULT_MARGIN,
+            xAxisLabel: options.xAxisLabel || '',
+            yAxisLabel: options.yAxisLabel || ''
+        };
         const svg = _createBaseSvg(containerId, config);
         const tooltip = _createTooltip(containerId);
 
         const x = d3.scaleBand()
             .range([0, config.width])
-            .domain(data.map(d => d.group))
-            .padding(0.2);
+            .domain(data.map(d => d.label))
+            .padding(0.3);
+
         svg.append("g")
             .attr("transform", `translate(0,${config.height})`)
             .call(d3.axisBottom(x))
             .selectAll("text")
-            .call(_applyRadiologyStyle, 'axis-tick')
-            .attr("transform", "translate(-10,0)rotate(-45)")
-            .style("text-anchor", "end");
+            .attr("transform", "translate(-5,0)rotate(-30)")
+            .style("text-anchor", "end")
+            .style("font-size", C.TICK_LABEL_FONT_SIZE);
 
+        const yMax = d3.max(data, d => d.value);
         const y = d3.scaleLinear()
-            .domain([0, d3.max(data, d => d.value)])
+            .domain([0, yMax > 0 ? yMax * 1.1 : 10])
             .range([config.height, 0]);
+
         svg.append("g")
             .call(d3.axisLeft(y).ticks(5))
-            .call(sel => sel.selectAll('.domain, .tick line').attr('stroke', C.GRIDLINE_COLOR))
-            .call(sel => sel.selectAll('.tick text').call(_applyRadiologyStyle, 'axis-tick'));
-
-        svg.selectAll("mybar")
+            .selectAll("text")
+            .style("font-size", C.TICK_LABEL_FONT_SIZE);
+            
+        svg.selectAll(".bar")
             .data(data)
-            .join("rect")
-            .attr("x", d => x(d.group))
-            .attr("y", d => y(d.value))
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", d => x(d.label))
             .attr("width", x.bandwidth())
-            .attr("height", d => config.height - y(d.value))
-            .attr("fill", C.NEW_PRIMARY_COLOR_BLUE);
+            .attr("y", d => y(0))
+            .attr("height", 0)
+            .attr("fill", C.AS_COLOR)
+            .on("mouseover", (event, d) => {
+                tooltip.style("opacity", 1)
+                       .html(`<strong>${d.label}</strong><br>${options.yAxisLabel || 'Wert'}: ${formatNumber(d.value, 0)}`)
+                       .style("left", (event.pageX + 15) + "px")
+                       .style("top", (event.pageY - 28) + "px");
+            })
+            .on("mouseout", () => tooltip.style("opacity", 0))
+            .transition()
+            .duration(C.ANIMATION_DURATION_MS)
+            .attr("y", d => y(d.value))
+            .attr("height", d => config.height - y(d.value));
     }
-    
-    function renderPieChart(containerId, data, isCompact = false) {
+
+    function renderPerformanceComparisonChart(containerId, data, labels, options = {}) {
         const config = {
-            width: C.DEFAULT_WIDTH,
-            height: isCompact ? C.DEFAULT_WIDTH : C.DEFAULT_HEIGHT,
-            margin: isCompact ? C.COMPACT_PIE_MARGIN : C.DEFAULT_MARGIN,
+            width: options.width || C.DEFAULT_WIDTH * 1.2,
+            height: options.height || C.DEFAULT_HEIGHT,
+            margin: options.margin || { top: 30, right: 30, bottom: 50, left: 120 },
+            yAxisLabel: options.yAxisLabel || 'Wert'
         };
-        const radius = Math.min(config.width, config.height) / 2 - Math.min(config.margin.top, config.margin.left);
-        d3.select(containerId).html('');
-        const svg = d3.select(containerId)
-            .append("svg")
-            .attr("width", config.width)
-            .attr("height", config.height)
-            .append("g")
-            .attr("transform", `translate(${config.width / 2},${config.height / 2})`);
-
-        const color = d3.scaleOrdinal().domain(data.map(d => d.label)).range(C.COLOR_SCHEMES.default);
-        const pie = d3.pie().value(d => d.value);
-        const data_ready = pie(data);
-
-        const arc = d3.arc().innerRadius(0).outerRadius(radius);
-
-        svg.selectAll('slices')
-            .data(data_ready)
-            .join('path')
-            .attr('d', arc)
-            .attr('fill', d => color(d.data.label))
-            .attr("stroke", "white")
-            .style("stroke-width", "2px");
-
-        const legend = svg.selectAll(".legend")
-            .data(data_ready)
-            .enter().append("g")
-            .attr("class", "legend")
-            .attr("transform", (d, i) => `translate(${(i % 2 === 0 ? -1 : 0.5) * (config.width / 2 - 40)}, ${Math.floor(i/2) * 20 + radius + 20})`);
-        
-        legend.append("rect")
-            .attr("width", 10)
-            .attr("height", 10)
-            .style("fill", d => color(d.data.label));
-
-        legend.append("text")
-            .attr("x", 15)
-            .attr("y", 10)
-            .text(d => `${d.data.label} (${d.data.value})`)
-            .call(_applyRadiologyStyle, 'legend');
-    }
-
-    function renderPerformanceComparisonChart(containerId, data, studyName) {
-        const config = { width: C.DEFAULT_WIDTH, height: C.DEFAULT_HEIGHT, margin: { top: 30, right: 20, bottom: 50, left: 100 } };
         const svg = _createBaseSvg(containerId, config);
         const tooltip = _createTooltip(containerId);
 
-        const y = d3.scaleBand()
-            .range([0, config.height])
-            .domain(data.map(d => d.metric))
-            .padding(0.4);
+        const groups = data.map(d => d.metric);
+        const subgroups = [labels.method1, labels.method2];
 
-        svg.append("g")
-            .call(d3.axisLeft(y))
-            .call(sel => sel.selectAll('.domain').remove())
-            .call(sel => sel.selectAll('.tick line').attr('stroke', 'none'))
-            .call(sel => sel.selectAll('.tick text').call(_applyRadiologyStyle, 'axis-tick').style('font-weight', 'bold'));
-
-        const x = d3.scaleLinear()
-            .domain([0, 1])
-            .range([0, config.width]);
+        const x = d3.scaleBand()
+            .domain(groups)
+            .range([0, config.width])
+            .padding(0.2);
 
         svg.append("g")
             .attr("transform", `translate(0, ${config.height})`)
-            .call(d3.axisBottom(x).ticks(5).tickFormat(d3.format(".0%")))
-            .call(sel => sel.selectAll('.domain').attr('stroke', C.GRIDLINE_COLOR))
-            .call(sel => sel.selectAll('.tick line').attr('stroke', C.GRIDLINE_COLOR).style('stroke-dasharray', ('2,2')))
-            .call(sel => sel.selectAll('.tick text').call(_applyRadiologyStyle, 'axis-tick'));
+            .call(d3.axisBottom(x).tickSizeOuter(0))
+            .selectAll("text")
+            .style("font-size", C.TICK_LABEL_FONT_SIZE);
+
+        const y = d3.scaleLinear()
+            .domain([0, 1])
+            .range([config.height, 0]);
+
+        svg.append("g")
+            .call(d3.axisLeft(y).ticks(5).tickFormat(d3.format(".0%")))
+            .selectAll("text")
+            .style("font-size", C.TICK_LABEL_FONT_SIZE);
         
-        svg.selectAll(".grid-line")
-           .data(x.ticks(5))
-           .enter().append("line")
-           .attr("class", "grid-line")
-           .attr("x1", d => x(d))
-           .attr("x2", d => x(d))
-           .attr("y1", 0)
-           .attr("y2", config.height)
-           .attr("stroke", C.GRIDLINE_COLOR)
-           .style('stroke-dasharray', ('2,2'));
+        const xSubgroup = d3.scaleBand()
+            .domain(subgroups)
+            .range([0, x.bandwidth()])
+            .padding(0.05);
 
         const color = d3.scaleOrdinal()
-            .domain(['Avocado Sign', studyName])
+            .domain(subgroups)
             .range([C.AS_COLOR, C.T2_COLOR]);
 
-        const ySubgroup = d3.scaleBand()
-            .domain(['Avocado Sign', studyName])
-            .range([0, y.bandwidth()])
-            .padding(0.1);
-
-        const groups = svg.selectAll(".metric-group")
+        svg.append("g")
+            .selectAll("g")
             .data(data)
             .join("g")
-            .attr("class", "metric-group")
-            .attr("transform", d => `translate(0, ${y(d.metric)})`);
-
-        groups.selectAll(".ci-line")
-            .data(d => d.values)
-            .join("line")
-            .attr("class", "ci-line")
-            .attr("x1", d => x(d.ci_lower))
-            .attr("x2", d => x(d.ci_upper))
-            .attr("y", d => ySubgroup(d.name) + ySubgroup.bandwidth() / 2)
-            .attr("stroke", d => color(d.name))
-            .attr("stroke-width", 1);
+            .attr("transform", d => `translate(${x(d.metric)}, 0)`)
+            .selectAll("rect")
+            .data(d => d.values.map(v => ({...v, key: d.metric})))
+            .join("rect")
+            .attr("x", d => xSubgroup(d.name))
+            .attr("width", xSubgroup.bandwidth())
+            .attr("fill", d => color(d.name))
+            .attr("y", d => y(0))
+            .attr("height", 0)
+            .on("mouseover", (event, d) => {
+                 tooltip.style("opacity", 1)
+                       .html(`<strong>${d.name} - ${d.key}</strong><br>${config.yAxisLabel}: ${formatCI(d.value, d.ci_lower, d.ci_upper, 1, true)}`)
+                       .style("left", (event.pageX + 15) + "px")
+                       .style("top", (event.pageY - 28) + "px");
+            })
+            .on("mouseout", () => tooltip.style("opacity", 0))
+            .transition()
+            .duration(C.ANIMATION_DURATION_MS)
+            .attr("y", d => y(d.value))
+            .attr("height", d => config.height - y(d.value));
             
-        groups.selectAll(".value-point")
+        const errorBars = svg.append("g")
+            .selectAll("g")
+            .data(data)
+            .join("g")
+            .attr("transform", d => `translate(${x(d.metric)}, 0)`)
+            .selectAll(".error-bar")
             .data(d => d.values)
-            .join("circle")
-            .attr("class", "value-point")
-            .attr("cx", d => x(d.value))
-            .attr("cy", d => ySubgroup(d.name) + ySubgroup.bandwidth() / 2)
-            .attr("r", C.POINT_RADIUS)
-            .attr("fill", d => color(d.name));
+            .join("g")
+            .attr("class", "error-bar");
+            
+        errorBars.append("line")
+            .attr("x1", d => xSubgroup(d.name) + xSubgroup.bandwidth() / 2)
+            .attr("x2", d => xSubgroup(d.name) + xSubgroup.bandwidth() / 2)
+            .attr("y1", d => y(d.ci_lower))
+            .attr("y2", d => y(d.ci_upper))
+            .attr("stroke", "black")
+            .attr("stroke-width", 1.5);
+    }
+    
+    async function getSvgBlob(svgElement) {
+        return new Promise((resolve, reject) => {
+            if (!svgElement) return reject(new Error("SVG-Element nicht gefunden."));
+            const serializer = new XMLSerializer();
+            let source = serializer.serializeToString(svgElement);
+            if (!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)) {
+                source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+            }
+            if (!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)) {
+                source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+            }
+            source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+            resolve(new Blob([source], { type: 'image/svg+xml;charset=utf-8' }));
+        });
     }
 
     async function convertSvgToPngBlob(svgElement, scale = 2) {
         return new Promise((resolve, reject) => {
-            const svgData = new XMLSerializer().serializeToString(svgElement);
-            const canvas = document.createElement("canvas");
-            const svgSize = svgElement.getBoundingClientRect();
-            canvas.width = svgSize.width * scale;
-            canvas.height = svgSize.height * scale;
-            const ctx = canvas.getContext("2d");
-            ctx.scale(scale, scale);
-
-            const img = new Image();
-            img.onload = () => {
-                ctx.drawImage(img, 0, 0);
-                canvas.toBlob((blob) => {
-                    if (blob) {
-                        resolve(blob);
-                    } else {
-                        reject(new Error("Canvas to Blob conversion failed."));
-                    }
-                }, "image/png");
-            };
-            img.onerror = (e) => {
-                reject(new Error(`Image loading failed: ${e}`));
-            };
-            img.src = "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgData)));
+            getSvgBlob(svgElement).then(blob => {
+                const url = URL.createObjectURL(blob);
+                const img = new Image();
+                img.onload = () => {
+                    const canvas = document.createElement('canvas');
+                    const bbox = svgElement.getBBox();
+                    canvas.width = bbox.width * scale;
+                    canvas.height = bbox.height * scale;
+                    const ctx = canvas.getContext('2d');
+                    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+                    URL.revokeObjectURL(url);
+                    canvas.toBlob(resolve, 'image/png');
+                };
+                img.onerror = (err) => {
+                    URL.revokeObjectURL(url);
+                    reject(err);
+                };
+                img.src = url;
+            }).catch(reject);
         });
     }
 
-
     return Object.freeze({
-        renderAgeDistribution,
-        renderPieChart,
+        renderBarChart,
         renderPerformanceComparisonChart,
-        convertSvgToPngBlob
+        convertSvgToPngBlob,
+        getSvgBlob
     });
 
 })();
